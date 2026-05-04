b53739d248ba0e0a21b6df3f40ed273774e26a52
DoubleSang <hdsa1564@gmail.com>
2026-05-04 11:32:44 +0900

    db create 안정화

---
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -0,0 +1,18 @@
+package com.back.domain.post.post.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity // 아래 구조대로 DB 테이블을 만들어야 한다.
+@Getter
+@Setter
+public class Post {
+    @Id //PRIMARY KEY
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+    private int id; //INT
+    private String title; //VARCHAR(255)
+}
